7826f0c0263eca16aa53ffa8ac3a767137888dc7
Shahprogrammer <shahprogrammer05@gmail.com>
2020-11-28 20:38:57 +0530

    Update __init__.py
    Created "append","get_node","get_last_node" method for SingleLinkedList class

--- SingleLinkedList/__init__.py
+++ SingleLinkedList/__init__.py
@@ -1,11 +1,13 @@
-from typing import Any,Union
+from typing import Any, Union
+
 
 class Node():
     """
     Node for Single Linked List
 
     """
-    def __init__(self,data:Any,next:Union['Node',None]=None) -> None:
+
+    def __init__(self, data: Any, next: Union['Node', None] = None) -> None:
         """
         Generates a Node for Single Linked List
 
@@ -14,10 +16,11 @@ class Node():
         :param next: Next Node of Single Linked List
         :type next: Union[Node,None]
         """
-        self._data=data
-        self._next=next
+        self._data = data
+        self._next = next
+
     @property
-    def data(self)->Any:
+    def data(self) -> Any:
         """
         Getter Method for data attribute
 
@@ -25,17 +28,19 @@ class Node():
         :rtype: Any
         """
         return self._data
+
     @data.setter
-    def data(self, data:Any):
+    def data(self, data: Any):
         """
         Setter Method for data attribute
 
         :param data: Value to be set for data attribute
         :type data: Any
         """
-        self._data=data
+        self._data = data
+
     @property
-    def next(self) -> Union['Node',None]:
+    def next(self) -> Union['Node', None]:
         """
         Getter Method for next attribute
 
@@ -43,25 +48,56 @@ class Node():
         :rtype: Union['Node',None]
         """
         return self._next
+
     @next.setter
-    def next(self, next:Union['Node',None]):
+    def next(self, next: Union['Node', None]):
         """
         Setter Method for next attribute
 
         :param next: Value to be set for next attribute
         :type next: Union['Node',None]
         """
-        self._next=next
+        self._next = next
+
+
 class SingleLinkedList():
     """
     Generates a Single Linked List
 
     """
-    def __init__(self,node:Union[Node,None]):
+
+    def __init__(self, node: Union[Node, None]):
         """
         Generates a Single Linked List
 
         :param node: Node of the Single Linked List
         :type node: Union[Node,None]
         """
-        self.node=node
+        self.node = node
+
+    def get_node(self, index) -> Union[Node, None]:
+        node = self.node
+        for i in range(0, index):
+            if node.next != None:
+                node = node.next
+            else:
+                node = None
+                break
+        return node
+
+    def get_last_node(self) -> Node:
+        node = self.node
+        if node != None:
+            while node.next != None:
+                node = node.next
+            return node
+        else:
+            return node
+
+    def append(self, data):
+        node = self.get_last_node()
+        if node != None:
+            node.next = Node(data=data)
+        else:
+            self.node = Node(data=data)
+        return None
