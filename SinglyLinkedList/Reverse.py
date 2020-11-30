2fa0d7b11fe55cb04cd7fa78e21b9061917b6738
Shahprogrammer <shahprogrammer05@gmail.com>
2020-11-30 19:27:55 +0530

    Update Reverse.py

--- SinglyLinkedList/Reverse.py
+++ SinglyLinkedList/Reverse.py
@@ -1,10 +1,9 @@
+from SinglyLinkedList import SinglyLinkedList as LinkedList
 """
     Reverse the SinglyLinkedList
 
     https://www.geeksforgeeks.org/reverse-a-linked-list/
 """
-from SinglyLinkedList import SinglyLinkedList as LinkedList
-
 class SinglyLinkedList(LinkedList):
     def reverse(self)->None:
         prev=None
